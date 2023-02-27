07b1fa2289a1f9af35db3c6318ab36d104dc518b
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2023-02-27 12:34:55 -0800

    update all

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  const myData = [
+    { id: 1, name: "Ishaan" },
+    { id: 2, name: "Bansal" },
+    { id: 3, name: "Vin" },
+    { id: 4, name: "Ayak" },
+  ];
+
+  // const addItem = (id) => {
+  //   let newPeople = people.fill((person) => person.id);
+  //   setPeople(newPeople);
+  // };
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        console.log(people);
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+            {/* <button onClick={() => addItem(id)}>add</button> */}
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear items
+      </button>
+      <button className="btn" onClick={() => setPeople([myData])}>
+        add all
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
